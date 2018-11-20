2a7948cc23eec4ec915891ab365d08f0807224cc
hkung77 <henrykung@live.ca>
2018-11-20 23:54:56 +0900

    add lighting change mesh material

--- js/main.js
+++ js/main.js
@@ -9,36 +9,34 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-// const light = new THREE.HemisphereLight(0xeeeeff, 0x777788, 0.75);
-// light.position.set(0.5, 1, 0.75);
-// scene.add(light);
+const light = new THREE.AmbientLight(0xffffff, 5.0);
 
 const controls = new THREE.OrbitControls(camera, renderer.domElement);
 
 // Create cube
 const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
 const boxMaterials = [
-  new THREE.MeshBasicMaterial({
+  new THREE.MeshPhongMaterial({
     map: new THREE.TextureLoader().load("img/grass.jpg"),
     side: THREE.DoubleSide
   }),
-  new THREE.MeshBasicMaterial({
+  new THREE.MeshPhongMaterial({
     map: new THREE.TextureLoader().load("img/grass.jpg"),
     side: THREE.DoubleSide
   }),
-  new THREE.MeshBasicMaterial({
+  new THREE.MeshPhongMaterial({
     map: new THREE.TextureLoader().load("img/grass.jpg"),
     side: THREE.DoubleSide
   }),
-  new THREE.MeshBasicMaterial({
+  new THREE.MeshPhongMaterial({
     map: new THREE.TextureLoader().load("img/grass.jpg"),
     side: THREE.DoubleSide
   }),
-  new THREE.MeshBasicMaterial({
+  new THREE.MeshPhongMaterial({
     map: new THREE.TextureLoader().load("img/grass.jpg"),
     side: THREE.DoubleSide
   }),
-  new THREE.MeshBasicMaterial({
+  new THREE.MeshPhongMaterial({
     map: new THREE.TextureLoader().load("img/grass.jpg"),
     side: THREE.DoubleSide
   })
