c5f5f2eff0235ace74fd792e8074f8b62d48e9e2
hkung77 <henrykung@live.ca>
2018-11-20 22:53:47 +0900

    added controls and mesh

--- js/main.js
+++ js/main.js
@@ -1,5 +1,3 @@
-// import THREE from "three.min.js";
-
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
   75,
@@ -8,17 +6,45 @@ const camera = new THREE.PerspectiveCamera(
   1000
 );
 const renderer = new THREE.WebGLRenderer();
-
 renderer.setSize(window.innerWidth, window.innerHeight);
-
 document.body.appendChild(renderer.domElement);
+
+// const light = new THREE.HemisphereLight(0xeeeeff, 0x777788, 0.75);
+// light.position.set(0.5, 1, 0.75);
+// scene.add(light);
+
+const controls = new THREE.OrbitControls(camera, renderer.domElement);
 
 // Create cube
 const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
-const boxMaterial = new THREE.MeshBasicMaterial({
-  color: 0xffffff,
-  wireframe: false
-});
+const boxMaterials = [
+  new THREE.MeshBasicMaterial({
+    map: new THREE.TextureLoader().load("img/grass.jpg"),
+    side: THREE.DoubleSide
+  }),
+  new THREE.MeshBasicMaterial({
+    map: new THREE.TextureLoader().load("img/grass.jpg"),
+    side: THREE.DoubleSide
+  }),
+  new THREE.MeshBasicMaterial({
+    map: new THREE.TextureLoader().load("img/grass.jpg"),
+    side: THREE.DoubleSide
+  }),
+  new THREE.MeshBasicMaterial({
+    map: new THREE.TextureLoader().load("img/grass.jpg"),
+    side: THREE.DoubleSide
+  }),
+  new THREE.MeshBasicMaterial({
+    map: new THREE.TextureLoader().load("img/grass.jpg"),
+    side: THREE.DoubleSide
+  }),
+  new THREE.MeshBasicMaterial({
+    map: new THREE.TextureLoader().load("img/grass.jpg"),
+    side: THREE.DoubleSide
+  })
+];
+
+const boxMaterial = new THREE.MeshBasicMaterial(boxMaterials);
 const cube = new THREE.Mesh(boxGeometry, boxMaterial);
 scene.add(cube);
 
@@ -32,7 +58,10 @@ window.addEventListener("resize", () => {
   camera.updateProjectionMatrix();
 });
 
-const update = () => {};
+const update = () => {
+  // cube.rotation.x += 0.01;
+  // cube.rotation.y += 0.01;
+};
 
 const render = () => {
   renderer.render(scene, camera);
